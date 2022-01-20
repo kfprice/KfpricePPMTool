c27016d40fbdb1776be893af2c00d0c03282baaf
Kemet.Price1@gmail.com <Kemet.Price1@gmail.com>
2022-01-20 17:37:23 -0500

    Refactor Project Controller

--- PPMToolFullStack/src/main/java/com/kfprice/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/kfprice/ppmtool/web/ProjectController.java
@@ -2,6 +2,7 @@ package com.kfprice.ppmtool.web;
 
 
 import com.kfprice.ppmtool.domain.Project;
+import com.kfprice.ppmtool.services.MapValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,19 +26,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if (result.hasErrors()) {
-
-            Map<String, String> errorMap = new HashMap<>();
-
-            for (FieldError error : result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
