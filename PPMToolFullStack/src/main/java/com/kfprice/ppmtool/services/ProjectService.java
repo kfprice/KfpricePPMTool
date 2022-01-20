31fabe8272968fcd1dfe29dbe1b3ffeb78c98990
Kemet.Price1@gmail.com <Kemet.Price1@gmail.com>
2022-01-20 17:57:51 -0500

    Custom Exceptions for Unique Project Identifiers

--- PPMToolFullStack/src/main/java/com/kfprice/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/kfprice/ppmtool/services/ProjectService.java
@@ -1,9 +1,12 @@
 package com.kfprice.ppmtool.services;
 
 import com.kfprice.ppmtool.domain.Project;
+import com.kfprice.ppmtool.exceptions.ProjectIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.kfprice.ppmtool.repositories.ProjectRepository;
+
+import java.util.Locale;
 
 @Service
 public class ProjectService {
@@ -12,10 +15,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-
-        //Logic
-
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
-
 }
