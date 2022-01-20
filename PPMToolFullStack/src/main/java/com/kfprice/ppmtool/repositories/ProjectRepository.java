1896a49cc441e65ab3879c0b5aaa2a3189a713bc
Kemet.Price1@gmail.com <Kemet.Price1@gmail.com>
2022-01-20 15:08:14 -0500

    main

---
+++ PPMToolFullStack/src/main/java/com/kfprice/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.kfprice.ppmtool.repositories;
+
+import com.kfprice.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
