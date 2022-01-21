7a436fd6f93dacfe5853add360f598bea9dfe4e4
Kemet.Price1@gmail.com <Kemet.Price1@gmail.com>
2022-01-21 16:56:50 -0500

    first react component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1 className="App-title">Welcome to PPMTool</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Dashboard />
+        <Dashboard />
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
