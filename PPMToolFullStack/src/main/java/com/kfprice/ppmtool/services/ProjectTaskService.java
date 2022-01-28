c755706e88da328bb2376a1365f200d71a1ff78c
Kemet.Price1@gmail.com <Kemet.Price1@gmail.com>
2022-01-27 23:33:37 -0500

    ***BUG FIX*** ProjectTask priority, projectIdentifier, PTSequence

--- PPMToolFullStack/src/main/java/com/kfprice/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/kfprice/ppmtool/services/ProjectTaskService.java
@@ -28,15 +28,17 @@ public class ProjectTaskService {
         Integer BacklogSequence = backlog.getPTSequence();
         BacklogSequence++;
 
+        backlog.setPTSequence(BacklogSequence);
+
         projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
         projectTask.setProjectIdentifier(projectIdentifier);
 
- /*       if(projectTask.getPriority()==0||projectTask.getPriority()==null){
-            projectTask.setPriority(3);
-        }*/
-
         if(projectTask.getStatus()==""||projectTask.getStatus()==null) {
             projectTask.setStatus("TO_DO");
+        }
+
+      if(projectTask.getPriority()==null){
+            projectTask.setPriority(3);
         }
 
         return projectTaskRepository.save(projectTask);
